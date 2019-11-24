38df27e43dd714786073d3a746a87200000667a7
jlumbard <jlumbard@uwo.ca>
2019-11-24 09:23:59 -0500

    added html titles

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,38 +1,3 @@
 
-async function getQuestionData()
-{
-    let header = new Headers({
-        'Access-Control-Allow-Origin':'*',
-        
-    });
 
-    let response = await fetch(
-        'https://smart-visitor-259902.appspot.com/question/',
-        { 
-            method: 'GET',
-            headers:header,
-        }
-    )
-    let data = await response.json();
-    console.log(data);
-    return data;
-}
 
-async function getUserData(ID)
-{
-    let header = new Headers({
-        'Access-Control-Allow-Origin':'*',
-        
-    });
-
-    let response = await fetch(
-        'https://smart-visitor-259902.appspot.com/user/' + ID,
-        { 
-            method: 'GET',
-            headers:header,
-        }
-    )
-    let data = await response.json();
-    console.log(data);
-    return data;
-}
